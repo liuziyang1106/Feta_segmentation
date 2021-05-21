0bcdc18b2a328cc72a07c3603b8027555cac547a
liuziyang1106 <liuziyang1106@sina.com>
2021-05-21 14:11:21 +0800

    feat(inference): add single and folder inference

--- inferrence.py
+++ inferrence.py
@@ -1,13 +1,12 @@
 
 import numpy as np
-import torch
+import torch,os,shutil
 import torch.nn.functional as F
 from model_zoo import UNet
 import nibabel as nib
-import SimpleITK as sitk
 
 
-def predict_img(net, full_img, device, out_threshold=0.5):
+def predict_mask(net, full_img, device, out_threshold=0.5):
     net.eval()
 
     img = torch.from_numpy(full_img)
@@ -20,30 +19,54 @@ def predict_img(net, full_img, device, out_threshold=0.5):
         probs = torch.sigmoid(output)
         probs = probs.squeeze(0)
         full_mask = probs.squeeze().cpu().numpy()
-    mask = (full_mask > out_threshold)
-    # mask = mask.transpose((1, 2, 3, 0))
-    output = np.argmax(mask, axis=0)
-    output = output.astype(np.int16)
-    return output
+    mask = np.argmax(full_mask > out_threshold, axis=0).astype(np.int16)
+    return mask
 
-
-if __name__ == "__main__":
-
-    model = "./runs/test_64*128*128/unet_best_model.pth.tar"
-    img_path = "data/imgs_crop/sub-015_T2w.nii.gz"
-
-    net = UNet(n_channels=1, n_classes=8)
+def Inference_single_image(net, model_ckpt, img_path, output_path='./'):
+    
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
    
     net.to(device=device)
-    net.load_state_dict(torch.load(model, map_location=device)['state_dict'])
+    net.load_state_dict(torch.load(model_ckpt, map_location=device)['state_dict'])
 
     img = nib.load(img_path)
     img_data = img.get_fdata()
     img_affine = img.affine
     img_data = img_data[np.newaxis, :]
-     
-    mask = predict_img(net=net,full_img=img_data,out_threshold=0.5,device=device)
-    inference_path = "./data/sub-015_pred.nii.gz"
+    sub_id = (img_path.split('/')[-1]).replace('T2w', 'pred')
+    print(sub_id)
+    mask = predict_mask(net=net,full_img=img_data,out_threshold=0.5,device=device)
+    inference_path = os.path.join(output_path, sub_id)
     predict_img = nib.Nifti1Image(mask, img_affine).to_filename(inference_path)
+
+def Inference_Folder_images(net, model_ckpt, folder_path, output_path='./'):
+    
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+   
+    net.to(device=device)
+    net.load_state_dict(torch.load(model_ckpt, map_location=device)['state_dict'])
+
+    for img_path in os.listdir(folder_path):
+        if 'T2w' in img_path:
+            img = nib.load(os.path.join(folder_path,img_path))
+            img_data = img.get_fdata()
+            img_affine = img.affine
+            img_data = img_data[np.newaxis, :]
+            sub_id = (img_path.split('/')[-1]).replace('T2w', 'pred')
+            print(sub_id)
+            mask = predict_mask(net=net,full_img=img_data,out_threshold=0.5,device=device)
+            inference_path = os.path.join(output_path, sub_id)
+            predict_img = nib.Nifti1Image(mask, img_affine).to_filename(inference_path)
+            shutil.copyfile(os.path.join(folder_path,img_path),os.path.join(output_path, img_path))
+
+
+if __name__ == "__main__":
+    model = "./runs/test_64*128*128/unet_best_model.pth.tar"
+    img_path = "data/imgs_crop/sub-015_T2w.nii.gz"
+    folder = "data/test/"
+    net = UNet(n_channels=1, n_classes=8)
+    # Inference_single_image(net, model, img_path)
+    Inference_Folder_images(net, model, folder,'data')
+
+    
 
