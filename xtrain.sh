b28b1c0b702f59d9c3c084d8d54c2b5e26d1ea54
liuziyang1106 <liuziyang1106@sina.com>
2021-05-21 12:32:28 +0800

    feat(add xtrain.sh and config file): add xtrain.sh and config file to set training

---
+++ xtrain.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+save_path=./runs/test
+CUDA_VISIBLE_DEVICES=0     python train.py     \
+--output_dir          ${save_path}             \
+--batch_size          2                        \
+--epochs              100                      \
+--lr                  1e-3                     \
+--print_freq          2                        \
