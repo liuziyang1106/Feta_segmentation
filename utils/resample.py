337b4ecb31fd1db9d82661d1bc6badc40721a836
liuziyang1106 <liuziyang1106@sina.com>
2021-05-27 09:39:29 +0800

    feat(utils resample): combine resample and center crop into single file

--- utils/resample.py
+++ utils/resample.py
@@ -1,5 +1,8 @@
 import os
+import nibabel as nib
+import numpy as np
 import SimpleITK as sitk
+from scipy import ndimage
 
 def resampleVolume(outspacing,vol):
     """
@@ -61,5 +64,59 @@ def resample_folder(folder_path, target_path, spacing=[0.5,0.5,0.5]):
         wriiter.SetFileName(os.path.join(target_path, file))
         wriiter.Execute(newvol)
 
-resample_folder("/home/liuziyang/workspace/FeTA/Pytorch-UNet/data/masks/"
-              , "/home/liuziyang/workspace/FeTA/Pytorch-UNet/data/mask_resample/")
+
+def center_crop(file_path, seg_path, sub_id, img_target_path, mask_target_path):
+    print(file_path)
+    raw_img, seg_img = sitk.Image(sitk.ReadImage(file_path)), sitk.Image(sitk.ReadImage(seg_path))
+    resample_img, resample_mask = resampleVolume([0.5,0.5,0.5] ,raw_img), resampleVolume([0.5,0.5,0.5],seg_img)
+    resample_img, resample_mask = sitk.GetArrayFromImage(resample_img), sitk.GetArrayFromImage(resample_mask)
+
+    assert resample_img.shape == resample_mask.shape
+    print(resample_img.shape, resample_mask.shape)
+
+    resample_img = np.pad(resample_img,(100,100) ,mode="constant")
+    resample_mask = np.pad(resample_mask,(100,100) ,mode="constant")
+
+    assert resample_img.shape == resample_mask.shape
+    print(resample_img.shape, resample_mask.shape)
+
+    center_coord = ndimage.measurements.center_of_mass(resample_img)
+    crop_size = 100
+    crop_img = resample_img[int(center_coord[0])-crop_size:int(center_coord[0])+crop_size
+                          ,int(center_coord[1])-crop_size:int(center_coord[1])+crop_size
+                          ,int(center_coord[2])-crop_size:int(center_coord[2])+crop_size
+                          ]
+
+    crop_seg = resample_mask[int(center_coord[0])-crop_size:int(center_coord[0])+crop_size
+                            ,int(center_coord[1])-crop_size:int(center_coord[1])+crop_size
+                            ,int(center_coord[2])-crop_size:int(center_coord[2])+crop_size
+                            ]
+
+    assert crop_img.shape == crop_seg.shape
+    print(crop_img.shape, crop_seg.shape)
+    crop_img, crop_seg = sitk.GetImageFromArray(crop_img), sitk.GetImageFromArray(crop_seg)
+    raw_path = os.path.join(img_target_path, 'img_resample_crop',sub_id+'_T2w.nii.gz')
+    seg_save_path = os.path.join(mask_target_path, 'mask_resample_crop',sub_id+'_dseg.nii.gz')
+
+    img_wriiter,seg_writer = sitk.ImageFileWriter(),sitk.ImageFileWriter()
+    img_wriiter.SetFileName(raw_path)
+    seg_writer.SetFileName(seg_save_path)
+    img_wriiter.Execute(crop_img)
+    seg_writer.Execute(crop_seg)
+    # raw_img = nib.Nifti1Image(crop_img, raw_affine).to_filename(raw_path)
+    # seg_img = nib.Nifti1Image(crop_seg, seg_affine).to_filename(seg_save_path)
+
+def main():
+    root_path = "/home/liuziyang/workspace/FeTA/data/raw_data"
+    for sub_folder in os.listdir(root_path):
+        data_folder = os.path.join(root_path, sub_folder, 'anat')
+        for files in os.listdir(data_folder):
+            if 'T2w.nii.gz' in files:
+                file_path = os.path.join(data_folder, files)
+            elif 'dseg.nii.gz' in files:
+                seg_path = os.path.join(data_folder, files)
+
+        center_crop(file_path, seg_path, sub_folder
+        , "/home/liuziyang/workspace/FeTA/Pytorch-UNet/data/"
+        , "/home/liuziyang/workspace/FeTA/Pytorch-UNet/data/")
+main()
